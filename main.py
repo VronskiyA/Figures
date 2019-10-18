3b17a630bfe1373d6a7f94f7b2129613b864b41b
VronskiyA <vronskiy2004@gmail.com>
2019-10-18 11:29:50 +0700

    Add task for Ivan

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def square(x, y, size, color, angle):
   # TODO: (Masha) Draw square
   pass
-  
+
+def triangle(x, y, size, color, angle):
+  # TODO: (Ivan) Draw triangle
+  pass
+
 def main():
   # Figure 1 (Masha)
+  pass
+
+  # Figure 2 (Ivan)
   pass
   
 if __name__=='__mane__':
