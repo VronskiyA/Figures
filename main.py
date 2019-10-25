e96a37013a2fc700a3f69d88fe8b68baca8fd713
VronskiyA <vronskiy2004@gmail.com>
2019-10-25 09:42:49 +0700

    yghnj

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-def square(x, y, size, color, angle):
-  # TODO: (Masha) Draw square
-  pass
+# Case 1
+#
+import turtle
+class Square:
+  def __init__(self, x, y, size, color, angle):
+    self.x = x
+    self.x = y
+    self.color = color
+    self.angle = angle
+  def picsgab(self):
+    turtle.penup()
+    turtle.goto()
+    turtle.pendown()
+    turtle.left(angle)
+    for i in range(3)
+      turtle.forward(size)
+      turtle.tight(90)
+      turtle.root.mainloop()
+square(100, 100, 100, 12, 13)
 
-def triangle(x, y, size, color, angle):
-  # TODO: (Ivan) Draw triangle
-  pass
 
-def main():
-  # Figure 1 (Masha)
-  pass
 
-  # Figure 2 (Ivan)
-  pass
-  
-if __name__=='__mane__':
-  main()
