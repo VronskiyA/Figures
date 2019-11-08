49c3c4139ded7bc60294278d86094ddb7ee7abcc
VronskiyA <56710165+VronskiyA@users.noreply.github.com>
2019-11-08 10:49:34 +0700

    Add functions

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 # Case 1
 #
 import turtle
-class Square:
-  def __init__(self, x, y, size, color, angle):
-    self.x = x
-    self.x = y
-    self.color = color
-    self.angle = angle
-  def picsgab(self):
-    turtle.penup()
-    turtle.goto()
-    turtle.pendown()
-    turtle.left(angle)
-    for i in range(3)
-      turtle.forward(size)
-      turtle.tight(90)
-      turtle.root.mainloop()
-square(100, 100, 100, 12, 13)
-
-
-
+def square(x, y, size, angle, color)
+pass
+def triangle(x, y, a, b, c, angle, color)
+pass
+def circle(x, y, r, color)
+pass
+def parallelogram(x, y, a, b, angle, color)
+pass
