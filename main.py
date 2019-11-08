d5119c233b519d8bacd6eaf6d48e99c05f2adf2a
mishagryznov <56710325+mishagryznov@users.noreply.github.com>
2019-11-08 10:50:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import turtle
 def square(x, y, size, angle, color)
 pass
-def triangle(x, y, a, b, c, angle, color)
+def triangle(x, y, a, b, c, angle, color) Misha
 pass
 def circle(x, y, r, color)
 pass
